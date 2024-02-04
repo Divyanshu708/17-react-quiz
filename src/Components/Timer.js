c702701bd8aa98171b3f6b29a940f4855b082341
divyanshu708 <divyanshumj756@gmail.com>
2024-02-05 02:08:05 +0530

    components added

---
+++ src/Components/Timer.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+export default function Timer({ dispatch, secondsRemaining }) {
+  console.log(secondsRemaining);
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = Math.floor(secondsRemaining % 60);
+
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        // console.log("tick");
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+}
